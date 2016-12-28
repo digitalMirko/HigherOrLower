c2b2133949853d49aa6c0755c2b5af940ca4c0a7
Mirko Cukich <mirkocukich@gmail.com>
2016-12-28 11:34:20 -0600

    Initial commit

---
+++ app/src/main/java/com/digitalmirko/higherorlower/MainActivity.java
@@ -0,0 +1,20 @@
+package com.digitalmirko.higherorlower;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void btnClicked(View view){
+
+            
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
