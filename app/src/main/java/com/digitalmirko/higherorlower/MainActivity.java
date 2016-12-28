527eb3cf37306007c4a32ae6529fbf5424788ef9
Mirko Cukich <mirkocukich@gmail.com>
2016-12-28 13:37:25 -0600

    Program Complete

--- app/src/main/java/com/digitalmirko/higherorlower/MainActivity.java
+++ app/src/main/java/com/digitalmirko/higherorlower/MainActivity.java
@@ -4,21 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+   int ranPick = ran();
 
     public void btnClicked(View view){
+       // Test to see if Random Number Generator is working
+       Log.i("Random Number", Integer.toString(ranPick));
 
-        Random r = new Random();
-        int randomNumber = r.nextInt(30 - 1 + 1) + 1;
-        Log.i("Random Number", Integer.toString(randomNumber));
+       EditText numGuessEditText = (EditText) findViewById(R.id.numGuessEditText);
+
+       if(Integer.parseInt(numGuessEditText.getText().toString()) < ranPick){
+                Toast.makeText(this, "Nope try again Higher", Toast.LENGTH_SHORT).show();
+            } else if (Integer.parseInt(numGuessEditText.getText().toString()) > ranPick){
+                Toast.makeText(this, "Nope try again Lower", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Bingo, you got it", Toast.LENGTH_SHORT).show();
+       }
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // Random Number Generator method
+    public int ran (){
+        Random r = new Random();
+        int randomNumber = r.nextInt(30 - 1 + 1) + 1;
+        return randomNumber;
     }
 }
