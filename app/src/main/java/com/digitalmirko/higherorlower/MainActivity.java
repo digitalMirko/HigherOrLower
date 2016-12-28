4e6c2b13d9a46b215c29ead504af7234733b7e7f
Mirko Cukich <mirkocukich@gmail.com>
2016-12-28 16:43:35 -0600

    Cleaned up code - Program Complete

--- app/src/main/java/com/digitalmirko/higherorlower/MainActivity.java
+++ app/src/main/java/com/digitalmirko/higherorlower/MainActivity.java
@@ -10,31 +10,45 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-   int ranPick = ran();
 
-    public void btnClicked(View view){
-       // Test to see if Random Number Generator is working
-       Log.i("Random Number", Integer.toString(ranPick));
+    int ranPick = ran();
 
-       EditText numGuessEditText = (EditText) findViewById(R.id.numGuessEditText);
+    // Toast reply
+    public void makeToast(String string) {
 
-       if(Integer.parseInt(numGuessEditText.getText().toString()) < ranPick){
-                Toast.makeText(this, "Nope try again Higher", Toast.LENGTH_SHORT).show();
-            } else if (Integer.parseInt(numGuessEditText.getText().toString()) > ranPick){
-                Toast.makeText(this, "Nope try again Lower", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Bingo, you got it", Toast.LENGTH_SHORT).show();
-       }
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
     }
+
+    public void btnClicked(View view) {
+
+        // Test to see if Random Number Generator is working
+        Log.i("Random Number", Integer.toString(ranPick));
+
+        EditText numGuessEditText = (EditText) findViewById(R.id.numGuessEditText);
+
+        int guessNum = Integer.parseInt(numGuessEditText.getText().toString());
+
+        if (guessNum < ranPick) {
+            makeToast("Nope try again Higher");
+
+        } else if (guessNum > ranPick) {
+            makeToast("Nope try again Lower");
+
+        } else {
+            makeToast("Bingo, you got it");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     // Random Number Generator method
-    public int ran (){
+    public int ran() {
         Random r = new Random();
-        int randomNumber = r.nextInt(30 - 1 + 1) + 1;
+        int randomNumber = r.nextInt(30) + 1;
         return randomNumber;
     }
 }
